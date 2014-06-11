3465222c2707de01d7b6e47d4eb1f1d28424553f
Carl S. Yestrau Jr. <spam@featureblend.com>
2014-06-10 19:35:29 -0700

    Update Base.js

--- src/js/models/Base.js
+++ src/js/models/Base.js
@@ -1,5 +1,7 @@
-define(['backbone'], function(Backbone) {
-    return Backbone.Model.extend({
+define(['underscore', 'backbone', 'mixins/modelcollection'], function(_, Backbone, modelcollectionMixin) {
+    var Model = Backbone.Model.extend({
         //TBD.
     });
+    _.extend(Model.prototype, modelcollectionMixin);
+    return Model;
 })
